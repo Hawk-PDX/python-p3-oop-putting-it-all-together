c62f0998988e0a7a17ab591ae5be4020c6b953d8
Garrett <hawkpdx@icloud.com>
2025-01-17 00:22:18 -0800

    completed lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
-class Book:
-    pass
+class Book():
     
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
         
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+    
+    def __repr__(self):
+        return f"Title: {self.title}, Pages: {self.page_count}"
+        
+    
+  
+    def turn_page(self):
+        print(f"Flipping the page...wow, you read fast!")
+    
